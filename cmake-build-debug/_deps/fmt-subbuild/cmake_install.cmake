fd6687dee99714b33fc32ede6107669630a9b0a8
Łukasz Arakelyan <lukaszarakelyan@gmail.com>
2024-06-29 14:01:45 +0200

    Dodanie bilbioteki fmt

--- cmake-build-debug/_deps/fmt-subbuild/cmake_install.cmake
+++ cmake-build-debug/_deps/fmt-subbuild/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/lukaseq/CLionProjects/Symulator banku
+# Install script for directory: /home/lukaseq/CLionProjects/Symulator banku/cmake-build-debug/_deps/fmt-subbuild
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -37,16 +37,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/lukaseq/CLionProjects/Symulator banku/cmake-build-debug/_deps/fmt-build/cmake_install.cmake")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -55,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/lukaseq/CLionProjects/Symulator banku/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/lukaseq/CLionProjects/Symulator banku/cmake-build-debug/_deps/fmt-subbuild/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
